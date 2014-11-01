952dfdb9f4d36065001bb2535d7869079ff02cda
William Short <darkhelmet125@csu.fullerton.edu>
2014-11-01 04:17:36 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -134,17 +134,25 @@ float _cellHeight;
 }
 
 -(void)updateCreatures {
+    
+    int numAlive = 0;
+    
     for (int i = 0; i < [_gridArray count]; ++i) {
         for (int j = 0; j < [_gridArray[i]count]; ++j) {
             Creature *currentCreature = _gridArray[i][j];
             if (currentCreature.livingNeighbors == 3) {
                 currentCreature.isAlive = TRUE;
+                ++numAlive;
             }
             else if (currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >= 4) {
                 currentCreature.isAlive = FALSE;
+                if (numAlive > 0) {
+                    --numAlive;
+                }
             }
         }
     }
+    _totalAlive = numAlive;
 }
 
 @end
